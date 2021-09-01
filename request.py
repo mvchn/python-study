92cc913629177a783b6216b0c7f9e7900f8b2a68
mvchn <movchan@gmail.com>
2021-09-01 19:28:19 +0300

    add request

---
+++ request.py
@@ -0,0 +1,5 @@
+friends = ['mat', 'tom', 'jane', 'eric']
+print(f"Party time for {friends.pop().title()}")
+print(f"Party time for {friends.pop().title()}")
+print(f"{friends.pop().title()} can not visit")
+print(f"Party time for {friends.pop().title()}")
